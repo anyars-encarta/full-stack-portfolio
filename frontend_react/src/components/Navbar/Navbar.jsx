cb726d80251a52036b650ed2553ce7eb4bbc8984
anyars-encarta <anyarsencarta@gmail.com>
2024-03-22 20:48:13 +0000

    Complete Navbar

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
+import { images } from '../../constants';
+import { navItems } from '../../constants/listItems';
 import './Navbar.scss';
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  console.log(toggle);
+
   return (
-    <div>Navbar</div>
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.logo} alt='logo' />
+      </div>
+
+      <ul className='app__navbar-links'>
+        {
+          navItems.map((navItem) =>
+            <li key={`link-${navItem.id}`} className='app__flex p-text'>
+              <div />
+              <a href={`#${navItem.id}`}>{navItem.title}</a>
+            </li>
+          )
+        }
+      </ul>
+
+      <div className='app__navbar-menu'>
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {
+                navItems.map((navItem) =>
+                  <li key={navItem.id}>
+                    <a href={`#${navItem.id}`} onClick={() => setToggle(false)}>{navItem.title}</a>
+                  </li>
+                )
+              }
+            </ul>
+          </motion.div>
+        )}
+      </div>
+    </nav>
   )
 }
 
---
+++ frontend_react/src/constants/listItems.js
@@ -0,0 +1,22 @@
+export const navItems = [
+    {
+        id: "home",
+        title: "Home",
+    },
+    {
+        id: "about",
+        title: "About",
+    },
+    {
+        id: "work",
+        title: "Work",
+    },
+    {
+        id: "skills",
+        title: "Skills",
+    }, 
+    {
+        id: "contact",
+        title: "Contact",
+    }, 
+]

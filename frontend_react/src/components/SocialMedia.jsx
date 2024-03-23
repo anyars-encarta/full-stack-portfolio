78ace642400c7a5b11f8268434b8409f9639585c
anyars-encarta <anyarsencarta@gmail.com>
2024-03-23 22:32:10 +0000

    Implement a higher order component

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -6,25 +6,25 @@ const SocialMedia = () => {
     return (
         <div className='app__social'>
             <div>
-                <BsGithub />
+                <BsGithub onClick={() => window.open('https://github.com/anyars-encarta', '_blank')} />
             </div>
             <div>
-                <BsLinkedin />
+                <BsLinkedin onClick={() => window.open('https://www.linkedin.com/in/anyars-yussif/', '_blank')} />
             </div>
             <div>
-                <BsMedium />
+                <BsMedium onClick={() => window.open('https://medium.com/@anyarsencarta', '_blank')} />
             </div>
             <div>
-                <FaFacebookF />
+                <FaFacebookF onClick={() => window.open('https://web.facebook.com/encartadegreat/', '_blank')} />
             </div>
             <div>
-                <BsInstagram />
+                <BsInstagram onClick={() => window.open('https://www.instagram.com/anyarsyussif/', '_blank')} />
             </div>
             <div>
-                <BsTwitter />
+                <BsTwitter onClick={() => window.open('https://twitter.com/anyarsencarta', '_blank')} />
             </div>
             <div>
-                <BsYoutube />
+                <BsYoutube onClick={() => window.open('www.youtube.com/@anyarsencarta', '_blank')} />
             </div>
         </div>
     )

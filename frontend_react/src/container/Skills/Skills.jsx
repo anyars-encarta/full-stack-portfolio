a4f725e1f5700f32a54207cff8b49956529b6563
anyars-encarta <anyarsencarta@gmail.com>
2024-03-26 09:44:28 +0000

    Use Tooltip instead of ReactTooltip

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { ReactTooltip } from 'react-tooltip';
+import { Tooltip } from 'react-tooltip';
 
 import { AppWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
@@ -54,7 +54,7 @@ const Skills = () => {
               className='app__skills-exp-item'
               key={experience.year}
             >
-              <div className='app__slkills-exp-year'>
+              <div className='app__skills-exp-year'>
                 <p className='bold-text'>{experience.year}</p>
               </div>
 
@@ -73,14 +73,14 @@ const Skills = () => {
                       <p className='p-text'>{work.company}</p>
                     </motion.div>
 
-                    <ReactTooltip
+                    <Tooltip
                       id={work.name}
                       effect='solid'
                       arrowColor='#fff'
                       className='skills-tooltip'
                     >
                       {work.desc}
-                    </ReactTooltip>
+                    </Tooltip>
                   </>
                 ))}
               </motion.div>

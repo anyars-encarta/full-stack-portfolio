8b287fa65978140049741a15e2a9d4b175b65c8a
anyars-encarta <anyarsencarta@gmail.com>
2024-03-28 05:31:35 +0000

    Add Tilt to project

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { Tilt } from 'react-tilt';
 import { motion } from 'framer-motion';
+
 import './About.scss';
 import { urlFor, client } from '../../client';
 import { AppWrap, MotionWrap } from '../../wrapper';
@@ -11,7 +13,7 @@ const About = () => {
     const query = '*[_type == "abouts"]';
 
     client.fetch(query)
-    .then((data) => setAbouts(data));
+      .then((data) => setAbouts(data));
   }, []);
 
   return (
@@ -19,18 +21,21 @@ const About = () => {
       <h2 className='head-text'>I know that <span>Good Apps</span><br />Means <span>Good Business</span></h2>
 
       <div className='app__profiles'>
+
         {abouts.map((about, index) => (
-          <motion.div
-            whileInView={{ opacity: 1 }}
-            whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.5, type: 'tween' }}
-            className='app__profile-item'
-            key={about.title + index}
-          >
-            <img src={urlFor(about.imgUrl)} alt={about.title} />
-            <h2 className='bold-text' style={{ marginTop: '20px' }}>{about.title}</h2>
-            <p className='p-text' style={{ marginTop: '10px' }}>{about.description}</p>
-          </motion.div>
+          <Tilt options={{ max: 45, scale: 1, speed: 450 }}>
+            <motion.div
+              whileInView={{ opacity: 1 }}
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.5, type: 'tween' }}
+              className='app__profile-item'
+              key={about.title + index}
+            >
+              <img src={urlFor(about.imgUrl)} alt={about.title} />
+              <h2 className='bold-text' style={{ marginTop: '20px' }}>{about.title}</h2>
+              <p className='p-text' style={{ marginTop: '10px' }}>{about.description}</p>
+            </motion.div>
+          </Tilt>
         ))}
       </div >
     </>
@@ -38,7 +43,7 @@ const About = () => {
 }
 
 export default AppWrap(
-  MotionWrap(About, 'app__about'), 
+  MotionWrap(About, 'app__about'),
   'about',
   'app__whitebg'
 );
--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Tilt } from 'react-tilt';
 import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { motion } from 'framer-motion';
 
@@ -15,12 +16,12 @@ const Work = () => {
 
   const handleWorkFilter = (category) => {
     setActiveFilter(category);
-    setAnimateCard([{y:100, opacity: 0}]);
-    
-    setTimeout(() => {
-      setAnimateCard([{y:0, opacity: 1}]);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
 
-      if(category === 'All') {
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
+
+      if (category === 'All') {
         setFilterWork(works);
       } else {
         setFilterWork(works.filter((work) => work.tags.includes(category)))
@@ -60,48 +61,50 @@ const Work = () => {
         className='app__work-portfolio'
       >
         {filterWork.map((work, index) => (
-          <div className='app__work-item app__flex' key={index}>
-            <div className='app__work-img app__flex'>
-              <img src={urlFor(work.imgUrl)} alt={work.name} />
+          <Tilt options={{ max: 45, scale: 1, speed: 450 }}>
+            <div className='app__work-item app__flex' key={index}>
+              <div className='app__work-img app__flex'>
+                <img src={urlFor(work.imgUrl)} alt={work.name} />
 
-              <motion.div
-                whileHover={{ opacity: [0, 1] }}
-                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
-                className='app__work-hover app__flex'
-              >
-                <a href={work.projectLink} target='_blank' rel='noreferrer'>
-                  <motion.div
-                    whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.90] }}
-                    transition={{ duration: 0.25 }}
-                    className='app__flex'
-                  >
-                    <AiFillEye />
-                  </motion.div>
-                </a>
+                <motion.div
+                  whileHover={{ opacity: [0, 1] }}
+                  transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                  className='app__work-hover app__flex'
+                >
+                  <a href={work.projectLink} target='_blank' rel='noreferrer'>
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.90] }}
+                      transition={{ duration: 0.25 }}
+                      className='app__flex'
+                    >
+                      <AiFillEye />
+                    </motion.div>
+                  </a>
 
-                <a href={work.codeLink} target='_blank' rel='noreferrer'>
-                  <motion.div
-                    whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.90] }}
-                    transition={{ duration: 0.25 }}
-                    className='app__flex'
-                  >
-                    <AiFillGithub />
-                  </motion.div>
-                </a>
-              </motion.div>
-            </div>
+                  <a href={work.codeLink} target='_blank' rel='noreferrer'>
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.90] }}
+                      transition={{ duration: 0.25 }}
+                      className='app__flex'
+                    >
+                      <AiFillGithub />
+                    </motion.div>
+                  </a>
+                </motion.div>
+              </div>
 
-            <div className='app__work-content app__flex'>
-              <h4 className='bold-text'>{work.title}</h4>
-              <p className='p-text' style={{ marginTop: 10 }}>{work.description}</p>
-            
-              <div className='app__work-tag app__flex'>
-                <p className='p-text'>{work.tags[0]}</p>
+              <div className='app__work-content app__flex'>
+                <h4 className='bold-text'>{work.title}</h4>
+                <p className='p-text' style={{ marginTop: 10 }}>{work.description}</p>
+
+                <div className='app__work-tag app__flex'>
+                  <p className='p-text'>{work.tags[0]}</p>
+                </div>
               </div>
             </div>
-          </div>
+          </Tilt>
         ))}
       </motion.div>
     </>
@@ -109,7 +112,7 @@ const Work = () => {
 }
 
 export default AppWrap(
-  MotionWrap(Work, 'app__works'), 
+  MotionWrap(Work, 'app__works'),
   'work',
   'app__primarybg'
 );

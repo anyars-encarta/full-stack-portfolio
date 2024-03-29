862a2dcb45c916b1026e8123c1a8ae36b0694646
anyars-encarta <anyarsencarta@gmail.com>
2024-03-29 10:18:26 +0000

    Chnage styling for company name in experiences

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -72,7 +72,7 @@ const Skills = () => {
                       key={work.name}
                     >
                       <h4 className='bold-text'>{work.name}</h4>
-                      <p className='p-text'>{work.company}</p>
+                      <p className='company'>{work.company}</p>
                       <p className='p-text'>{work.desc}</p>
                     </motion.div>
 

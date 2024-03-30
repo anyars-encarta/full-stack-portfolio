fe0e5c95f6f9dc6b8c2ad67013546747df5350c0
anyars-encarta <anyarsencarta@gmail.com>
2024-03-30 09:02:40 +0000

    Show live icon only when page is deployed

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -54,7 +54,7 @@ const Work = () => {
           </div>
         ))}
       </div>
-      
+
       <p>{filterWork.length} projects match the {activeFilter} category</p>
 
       <motion.div
@@ -73,16 +73,18 @@ const Work = () => {
                   transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
                   className='app__work-hover app__flex'
                 >
-                  <a href={work.projectLink} target='_blank' rel='noreferrer'>
-                    <motion.div
-                      whileInView={{ scale: [0, 1] }}
-                      whileHover={{ scale: [1, 0.90] }}
-                      transition={{ duration: 0.25 }}
-                      className='app__flex'
-                    >
-                      <AiFillEye />
-                    </motion.div>
-                  </a>
+                  {work.projectLink === 'Not Deployed' ? '' :
+                    <a href={work.projectLink} target='_blank' rel='noreferrer'>
+                      <motion.div
+                        whileInView={{ scale: [0, 1] }}
+                        whileHover={{ scale: [1, 0.90] }}
+                        transition={{ duration: 0.25 }}
+                        className='app__flex'
+                      >
+                        <AiFillEye />
+                      </motion.div>
+                    </a>
+                  }
 
                   <a href={work.codeLink} target='_blank' rel='noreferrer'>
                     <motion.div
